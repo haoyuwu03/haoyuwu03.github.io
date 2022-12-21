c1982586c5b8b6a1f95b6f1db2f2565a8b67227e
Cindy Wu <hw286@duke.edu>
2022-12-21 16:32:13 -0500

    submit function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("code");
 form.addEventListener("submit", function(event){
-    answer = form.elements['answer']
+    const answer = form.elements['answer']
     print(answer)
 });
